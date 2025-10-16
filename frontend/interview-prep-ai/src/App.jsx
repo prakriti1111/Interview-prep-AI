ff2f111cb3615f976e734656d0c9abcadc12e7ea
parkriti1111 <prakritigupta468@gmail.com>
2025-10-16 20:09:39 +0530

    Landing page layout

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Toaster} from 'react-hot-toast';
+
+
+import LandingPage from "./pages/Home/LandingPage.jsx";
+import Dashboard from "./pages/Home/Dashboard.jsx";
+import Interviewprep from "./pages/InterviewPrep/InterviewPrep.jsx";
+
 
 const App = () => {
   return (
     <>
+      <div>
+        <Router>
+          <Routes>
+            {/* Deafult Page */}
+            <Route path='/' element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/interviewprep/:sessionId" element={<Interviewprep />} />
+          </Routes>
+        </Router>
+
+        <Toaster toastOptions={{
+          className: "",
+          style: {
+            fontSize: '13px',
+          },
+        }} 
+      />
+      </div>
     </>
   )
 }
---
+++ frontend/interview-prep-ai/src/components/Inputs/Input.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Input = ({value, onChange , label , placeholder , type}) => {
+
+  return (
+    <div>Input</div>
+  )
+}
+
+export default Input
