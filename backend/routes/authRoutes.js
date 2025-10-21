941cba8447445abc4d4da23c3c32408c6ac393bd
parkriti1111 <prakritigupta468@gmail.com>
2025-10-21 19:16:11 +0530

    initial backend setuo and basic files

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {registerUser , loginUser , getUserProfile} = require("../controllers/authController");
+const {protect} = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+// Auth Routes 
+
+router.post("/register", registerUser); //Register User
+router.post("/login", loginUser);
+router.get("/profile", protect , getUserProfile);
+
+module.exports = router;
